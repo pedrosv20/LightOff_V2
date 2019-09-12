c24a05d3774d9d6388fc4faa8d7ec5c02421472d
Pedro <pedro1silva1vargas@gmail.com>
2019-09-12 09:10:19 -0300

    Initial Commit

---
+++ LightOff_V2Tests/LightOff_V2Tests.swift
@@ -0,0 +1,34 @@
+//
+//  LightOff_V2Tests.swift
+//  LightOff_V2Tests
+//
+//  Created by Pedro Vargas on 12/09/19.
+//  Copyright © 2019 Pedro Vargas. All rights reserved.
+//
+
+import XCTest
+@testable import LightOff_V2
+
+class LightOff_V2Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
